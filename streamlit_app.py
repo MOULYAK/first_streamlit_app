efc74c3b3a751edbd40bfbea09428d697aaf9862
MOULYAK <moulyakotha2@gmail.com>
2022-04-26 15:01:47 +0530

    Update streamlit_app.py
    added text and header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title("My Mom's New Healthy Diner")
+streamlit.header("Breakfast Menu")
+streamlit.text("Omega 3 $ Blue Berry oatmeal")
+streamlit.text("Poha")
+streamlit.text("Milk")
